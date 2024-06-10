3a7e1ab0382dda311245069ffde0b7fd5dec84fc
HansStev <6182201087@student.unpar.ac.id>
2024-06-10 06:58:05 +0000

    pukiq

--- src/RemoveItemCommand.java
+++ src/RemoveItemCommand.java
@@ -2,18 +2,18 @@ public class RemoveItemCommand implements Command {
     private ShoppingCart cart;
     private Item item;
 
-    public RemoveItemCommand(ShoppingCart cart, Item item) {
+    public RemoveItemCommand(ShoppingCart cart, Item item) {    //executor dari fitur remove
         this.cart = cart;
         this.item = item;
     }
 
     @Override
-    public void execute() {
+    public void execute() {     //melakukan execute  command remove
         cart.removeItem(item);
     }
 
     @Override
-    public void undo() {
+    public void undo() {    //jika melakukan undo maka akan mengembalikan barang yang sebelumnya diremove
         cart.addItem(item);
     }
 }
